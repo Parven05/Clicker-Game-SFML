211f0005fa970469e5f1a52153b6758b1fb4ac87
Parven <parvendranofficial05@gmail.com>
2024-05-22 21:12:45 +0800

    added texture and audio

--- Clicker-Game/Clicker-Game/Clicker-Game.cpp
+++ Clicker-Game/Clicker-Game/Clicker-Game.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <string>
 
 int main()
@@ -15,10 +16,17 @@ int main()
     sf::Text text;
     sf::Font font;
 
+    sf::Sprite backgroundSprite;
+    sf::Texture background;
+    sf::Texture button;
+
+     sf::SoundBuffer buffer;
+     sf::Sound clickSound;
+
     settings.antialiasingLevel = 8;
 
     // Window
-    window.create(sf::VideoMode(500, 500), "Click Game", sf::Style::Default, settings);
+    window.create(sf::VideoMode(500, 500), "Click Game", sf::Style::Titlebar | sf::Style::Close, settings);
     window.setVerticalSyncEnabled(true);
     window.setFramerateLimit(60);
 
@@ -26,49 +34,80 @@ int main()
     circle.setPosition(100, 100);
     sf::Vector2f circlePosition = circle.getPosition() + sf::Vector2f(circle.getRadius(), circle.getRadius());
 
-    //add Text
-    if (!font.loadFromFile("Montserrat.ttf"))
+    // add Text
+    if (!font.loadFromFile("Montserrat-Medium.ttf"))
     {
         std::cout << "Font not set" << std::endl;
     }
 
+    // add texture
+    if (!background.loadFromFile("Background.jpg"))
+    {
+        std::cout << "Texture not set" << std::endl;
+    }
+
+
+    if (!button.loadFromFile("Button.png"))
+    {
+        std::cout << "Texture not set" << std::endl;
+    }
+
+    // add sound
+    if (!buffer.loadFromFile("Click.wav"))
+    {
+        std::cout << "Sound not set" << std::endl;
+    }
+
     text.setFont(font);
     text.setString("Score: 0");
-    text.setFillColor(color.Yellow);
-    text.setCharacterSize(50);
-    text.setStyle(text.Bold);
-    text.setPosition(145, 20);
+    text.setFillColor(color.Black);
+    text.setCharacterSize(40);
+    //text.setStyle(text.Bold);
+    text.setPosition(162, 20);
 
     // score initialize
     int scoreint = 0;
 
+    // set texture
+    button.setSmooth(true);
+    backgroundSprite.setTexture(background);
+    circle.setTexture(&button);
+
+    // set sound
+    clickSound.setBuffer(buffer);
+
+    bool isPressed = false;
+
     while (window.isOpen())
     {
-        // Mouse position
-        sf::Vector2i mousePositionint = mouse.getPosition(window);
-        sf::Vector2f mousePosition = static_cast<sf::Vector2f>(mousePositionint);
-
-        // Dot Product
-        sf::Vector2f toMouseVector = mousePosition - circlePosition; // From circle to mouse vector
-        float dotVector = toMouseVector.x * toMouseVector.x + toMouseVector.y * toMouseVector.y;
-
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            if (mouse.isButtonPressed(mouse.Left))
+            if (mouse.isButtonPressed(mouse.Left) && isPressed == false)
             {
+                // Mouse position
+                sf::Vector2i mousePositionint = mouse.getPosition(window);
+                sf::Vector2f mousePosition = static_cast<sf::Vector2f>(mousePositionint);
+
+                // Dot Product
+                sf::Vector2f toMouseVector = mousePosition - circlePosition; // From circle to mouse vector
+                float dotVector = toMouseVector.x * toMouseVector.x + toMouseVector.y * toMouseVector.y;
+
                 if (dotVector <= circle.getRadius() * circle.getRadius())
                 {
                     circle.setFillColor(color.Red);
+                    clickSound.play();
+                    isPressed = true;
                     scoreint++;
                     text.setString("Score: " + std::to_string(scoreint));
                 }
             }
             else
             {
-                circle.setFillColor(color.White);
+                circle.setFillColor(color.Green);
+                isPressed = false;
                 // text.setString("Hmm");
             }
 
@@ -76,11 +115,13 @@ int main()
         }
 
 
-        window.clear();
+        window.clear(sf::Color(238, 123, 104));
 
-        window.draw(text);
         // write code here
+       // window.draw(button);
+        window.draw(backgroundSprite);
         window.draw(circle);
+        window.draw(text);
         window.display();
     }
 
